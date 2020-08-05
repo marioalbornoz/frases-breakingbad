5327805097f3f8c4c96f307f90f74599fff53428
marioalbornoz <36382828+marioalbornoz@users.noreply.github.com>
2020-08-05 00:48:16 -0400

    loading> true

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 export const useFetch = (url) => {
     const [state, setstate] = useState({ data: null, loading: true, error: null });
     useEffect(() => {
+      setstate({loading:true, error:null})
       fetch(url)
         .then((resp) => resp.json())
         .then((data) => {
